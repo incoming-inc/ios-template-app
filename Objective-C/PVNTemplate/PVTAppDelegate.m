7f5eaf05b89622a5cdbf42dc63156d569ed3d171
Sebastien Ardon <sebastien@incoming-media.com>
2020-03-12 13:19:39 +1100

    Documentation update

--- Objective-C/PVNTemplate/PVTAppDelegate.m
+++ Objective-C/PVNTemplate/PVTAppDelegate.m
@@ -48,14 +48,14 @@
     return YES;
 }
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
 {
-    if ([ISDKAppDelegateHelper handleOpenURL:url sourceApplication:sourceApplication annotation:annotation] == NO)
-    {
+    if ([ISDKAppDelegateHelper application:application openURL:url options:nil] == NO) {
         // perform handling of your app URL here
     }
     return YES;
 }
+
 
 - (void) application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler{
     [ISDKAppDelegateHelper application:application performFetchWithCompletionHandler:completionHandler];
